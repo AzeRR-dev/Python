3dad629d62fb10ee8a5e3dbbc10d5ddde5e22907
AzeRR-Dev <79335454+AzeRR-dev@users.noreply.github.com>
2021-03-02 13:31:46 +0300

    Add files via upload

---
+++ Market/enayıBulma.py
@@ -0,0 +1,18 @@
+print("Enayi bulma programına hoşgeldiniz.")
+print("Seçebileceğiniz Enayiler:Efe,Boran")
+
+
+
+
+enayı=input("Seçmek istediğiniz Enayi:")
+
+if enayı=="efe":
+  print("Kendileri enayilerin tanrısıdır.1 dolar için 70000 dolar verme gibi sıradısı  yeteneklerri vardir.")
+
+
+if enayı=="boran":
+  print("kendileri enayilik kavramını felsefe dökmüştür. Bu sanatı yürekten yapan Boran'ı buraya almamak aptallık olurdu.")
+
+
+else:
+  print ("Dünyada boran ve efe dışında enayi yoktur.")
